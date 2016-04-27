7c4b6228948cda269461feebba37ac9f5fe79ac2
keivn c <keivnc@keivns-MacBook-Pro.local>
2016-04-26 17:50:14 -0700

    added protocol funcs, fixed msg framing

--- serial_msg_send/serial_msg_send.ino
+++ serial_msg_send/serial_msg_send.ino
@@ -5,17 +5,16 @@
 #define STARTED 1
 #define SYNC_CHARACTER '{'
 #define TERMINATOR_CHARACTER '}'
+#define INPUT_LENGTH 250
 
-unsigned char incomingByte;   // for incoming serial data
-unsigned char reliableLength;
+unsigned char incomingByte;   // read serial input
 unsigned char commandIndex = 0;
 unsigned char commandState = CLEAR;
-unsigned char reliableMode = false;
-
-#define INPUT_LENGTH 250
+//unsigned char reliableLength;
+//unsigned char reliableMode = false;
 unsigned char commandBuffer[INPUT_LENGTH + 2];
 
-// Singleton instance of the radio driver
+// instance of the radio driver
 RH_RF22 rf22;
 
 void setup() 
@@ -25,66 +24,217 @@ void setup()
     Serial.println("init failed");
 }
 
+void framePacket (byte * * ppFramedData, word * pFramedLength, byte * pPacketData, byte packetLength)
+{
+  word index;
+  byte * pFramedData;
+  byte * pData;
+  word calculatedLength;
+
+  for (index = 0, calculatedLength = 0, pData = pPacketData; index < packetLength; index++, pData++)
+  {
+    switch (*pData)
+    {
+        case '%':
+        case '{':
+        case '}':
+            calculatedLength += 2;
+            break;
+        default:
+            calculatedLength++;
+    }
+  }
+  
+  calculatedLength += 2;  // add length of opening and closing curly braces
+  
+  pFramedData = (byte *) malloc (calculatedLength + 2);
+  
+  *ppFramedData = pFramedData;
+  
+  *pFramedData++ = '{';
+  
+  for (index = 0, pData = pPacketData; index < packetLength; index++, pData++)
+  {
+      switch (*pData)
+      {
+          case '%':
+              *pFramedData++ = '%';
+              *pFramedData++ = '%';
+              break;
+          case '{':
+              *pFramedData++ = '%';
+              *pFramedData++ = '[';
+              break;
+          case '}':
+              *pFramedData++ = '%';
+              *pFramedData++ = ']';
+              break;
+          default:
+              *pFramedData++ = *pData;
+              break;
+      }
+  }
+  *pFramedData++ = '}';
+  *pFramedLength = calculatedLength;
+}
+
+void extractPacket (byte * * ppPacketData, byte * pPacketLength, byte * pFramedPacketData, word framePacketLength, word * pConsumedLength)
+{
+  word index;
+  byte * pFramedData;
+  byte * pEndOfFramedData;
+  word calculatedLength = 0;
+  byte * pUnframedData;
+  
+  *ppPacketData = NULL;
+  
+  pEndOfFramedData = pFramedPacketData + framePacketLength;
+  pFramedData = pFramedPacketData;
+
+  if ('{' == *pFramedData)
+  {
+      pFramedData++;
+
+      for ( ; pFramedData < pEndOfFramedData; pFramedData++)
+      {
+          if ('%' == *pFramedData)
+          {
+              pFramedData++;
+          }
+          else
+          {
+              if ('}' == *pFramedData)
+              {
+                  *pConsumedLength = (pFramedData - pFramedPacketData) - 1; // calculate how many bytes we consumed
+                  goto FoundClosingFrame;
+              }
+          }
+          calculatedLength++;
+      }
+  // if we exit the for loop without finding the closing brace (frame) this is an error
+      return;
+        
+
+FoundClosingFrame:
+        
+// NTR check for alloc failure
+      pUnframedData = (byte *) malloc (calculatedLength + 2);
+      *ppPacketData = pUnframedData;
+      *pPacketLength = calculatedLength;
+      
+      pFramedData   = pFramedPacketData + 1;  // start after opening brace
+  
+      for (index = 0; index < calculatedLength; index++)
+        {
+          if ('%' == *pFramedData)
+          {
+              pFramedData++;
+              
+              switch (*pFramedData++)
+              {
+                case '[':
+                  *pUnframedData++ = '{'; 
+                  break;
+                case ']':
+                  *pUnframedData++ = '}';
+                  break;
+                default:
+                  *pUnframedData++ = *pFramedData;
+                  break;
+              }
+              pFramedData++;
+          }
+          else
+          {
+            *pUnframedData++ = *pFramedData++;
+          }
+        }
+    }
+}
+
 void loop()
 {
 
-  // Write to Serial
-  // Send a message to a rf22_mesh_server
-  // A route to the destination will be automatically discovered.
-  while(Serial.available() > 0) {
-     incomingByte = Serial.read();
-
-     if (CLEAR == commandState) {
-       if (SYNC_CHARACTER == incomingByte) {  // look for start of command sync character
+  byte *pPacketData;
+  byte packetLength;
+  word consumedBytes;
+  
+  byte * pFramedPacketData;
+  word framedPacketLength;
+  
+  // read from serial
+  // send a message to a AP/server
+  if (Serial.available() > 0)
+  {
+    
+    incomingByte = Serial.read();
+    // framePacket(byte incomingByte, byte sizeof(incomingByte));
+    
+    if (commandState == CLEAR)
+    {
+       if (SYNC_CHARACTER == incomingByte) // look for start of command sync character
+       {
          commandState = STARTED;
-         continue;
        }
      }
 
-     if (STARTED == commandState) {      
-       if (TERMINATOR_CHARACTER == incomingByte) {  // look for end of command terminator character
-         commandBuffer[commandIndex] = 0;  // null terminate the command buffer
-         Serial.println("[found end]");
-         rf22.send(commandBuffer, commandIndex);
+     if (commandState == STARTED)
+     {      
+       if (TERMINATOR_CHARACTER == incomingByte) // look for end of command terminator character
+       {
+        
+         commandBuffer[commandIndex] = incomingByte;  // store a command character in buffer
+         commandBuffer[commandIndex +1] = 0;  // null terminate the command buffer
+//         Serial.println("[found end]");
+//         Serial.println((char*)commandBuffer);
+//         extractPacket(& pPacketData, & packetLength, commandBuffer, commandIndex, & consumedBytes);
+//         rf22.send(pPacketData, packetLength);
+//         rf22.waitPacketSent();
+//         free (pPacketData);
+         
+         rf22.send(commandBuffer, commandIndex +1);
+         rf22.waitPacketSent();
+         Serial.print("sending: ");
+         Serial.print((char*)commandBuffer);
+         Serial.println(" ");
          commandIndex = 0;
          commandState = CLEAR;
-         return;
        }
-       else {
+       else
+       {
          commandBuffer[commandIndex] = incomingByte;  // store a command character in buffer
          commandIndex++;
-         if (INPUT_LENGTH < commandIndex) {
+         if (INPUT_LENGTH < commandIndex)
+         {
            Serial.println("{overflow}");
            commandIndex = 0;
            commandState = CLEAR;
-           return;
          }
        }
      }      
   }
-  
-//  Serial.println("Sending to rf22_server");
-//  // generate max msg len data
-//  uint8_t data[RH_RF22_MAX_MESSAGE_LEN];
-//  for (int i = 0; i < sizeof(data); i++)
-//  {
-//      data[i] = 'a';
-//  }
-//  
-//  rf22.send(data, sizeof(data));
-  
-//  rf22.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[INPUT_LENGTH + 2];
-  uint8_t len = sizeof(buf);
 
-  // Should be a reply message for us now   
-  if (rf22.recv(buf, &len))
+  if (rf22.available())
   {
+    // Now wait for a reply
+    uint8_t buf[INPUT_LENGTH +2];
+    uint8_t len = INPUT_LENGTH;
+  
+    // Should be a reply message for us now
+    if (rf22.recv(buf, &len))
+    {
+      
       buf[len] = '\0';
-      Serial.print("{");
+      Serial.print("=");
       Serial.print((char*)buf);
-      Serial.println("}");
+      Serial.println("=");
+  
+//      framePacket(& pFramedPacketData, & framedPacketLength, buf, len);
+//      *(pFramedPacketData + framedPacketLength) = 0;
+//      Serial.println((const char*)pFramedPacketData);
+//      free (pFramedPacketData);
+      
+    }
   }
 }
 
